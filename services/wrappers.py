0f73910e4f958b5c52f930f32a8d13e14a8f840c
Mascherino <code@keedosuul.de>
2025-05-08 03:53:45 +0200

    Repair decorators

--- services/wrappers.py
+++ services/wrappers.py
@@ -1,23 +1,24 @@
-import os
 from functools import wraps
+from typing import Any
 
 from flask import current_app, flash, redirect, url_for
 from flask_login import current_user, login_required, login_user
 
-from extensions import db
+from database import db
 from models import User
-
-DEV_USER_EMAIL = os.getenv("DEV_USER_EMAIL", "dev@example.com")
-DEV_USER_PASSWORD = os.getenv("DEV_USER_PASSWORD", "dev")
+from services.const import DEV_USER_EMAIL, DEV_USER_PASSWORD
+from session_timeout import DemoTimeout
 
 
 def login_required_dev(f):
     @wraps(f)
-    def decorated_function(*args, **kwargs):
+    def decorated_function(*args, **kwargs) -> Any:
         if current_app.config["DEVELOPMENT_MODE"]:
             if not current_user.is_authenticated:
                 # Get or create dev user
-                dev_user = User.query.filter_by(id=DEV_USER_EMAIL).first()
+                dev_user: User | None = (
+                    db.session.query(User).filter_by(id=DEV_USER_EMAIL).first()
+                )
                 if not dev_user:
                     dev_user = User(
                         id=DEV_USER_EMAIL, name="Developer", is_admin=True
@@ -36,9 +37,14 @@ def login_required_dev(f):
 
 def restrict_demo_access(f):
     @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if current_user.is_authenticated and demo_timeout.is_demo_user(
-            current_user.id
+    def decorated_function(*args, **kwargs) -> Any:
+        demo_timeout: DemoTimeout | None = current_app.extensions.get(
+            "demo_timeout"
+        )
+        if (
+            current_user.is_authenticated
+            and demo_timeout
+            and demo_timeout.is_demo_user(current_user.id)
         ):
             flash("Demo users cannot access this page.")
             return redirect(url_for("dashboard"))
